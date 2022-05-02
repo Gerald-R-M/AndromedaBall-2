ba60610df9414565dd3f7ccc481257f639ad44f2
Gerald-R-M <gerald.monnecka1@marist.edu>
2022-05-02 14:55:44 -0400

    Voiceline Logic
    Preliminary voiceline logic complete testing needed to see if everything ties together properly. Still need to layout a way for the gameplay to not be possible while the voicelines are being played

--- Assets/Scripts/VoicelineManager.cs
+++ Assets/Scripts/VoicelineManager.cs
@@ -3,42 +3,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class VoicelineManager : MonoBehaviour
 {
+    [SerializeField] private Text subtitleRef;
     [SerializeField] private Voiceclip[] firstLines;
     [SerializeField] private Voiceclip[] secondLines;
     [SerializeField] private Voiceclip finalLine;
-    private finalClip _fullClip;
+    private FinalClip _fullClip;
     private bool _firstPlayed;
     private bool _secondPlayed;
     private bool _finalPlayed;
     private AudioSource _sourceRef;
-    
+    private float _timePassed;
     [Tooltip("The amount of time (in seconds) that should pass between each line"),SerializeField] private float delay;
 
     // Start is called before the first frame update
     void Start()
     {
-        _fullClip = new finalClip(ClipSelector(firstLines), ClipSelector(secondLines), finalLine);
+        //TODO prevent all gameplay while voicelines are being played
+        _fullClip = new FinalClip(ClipSelector(firstLines), ClipSelector(secondLines), finalLine);
         _sourceRef = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!_sourceRef.isPlaying && !_firstPlayed)
+        if (!_sourceRef.isPlaying && _timePassed >= delay)
         {
-            playClip(finalClip.firstLine);
+            if(!_firstPlayed)
+            {
+                PlayClip(_fullClip.firstLine);
+                _firstPlayed = true;
+            }
+            else if (_firstPlayed && !_secondPlayed)
+            {
+                PlayClip(_fullClip.secondLine);
+                _secondPlayed = true;
+            }
+            else if (_firstPlayed && _secondPlayed && !_finalPlayed)
+            {
+                PlayClip(_fullClip.finalLine);
+                _finalPlayed = true;
+            }
+            _timePassed += Time.deltaTime;
+        }
+        else
+        {
+            _timePassed = 0;
         }
     }
 
-    void playClip(Voiceclip clip)
+    private void PlayClip(Voiceclip clip)
     {
-        //TODO pass string to UI
-        _sourceRef.PlayOneShot(clip.voiceline, clip.VolumeScale);
+        subtitleRef.text = clip.subtitle;
+        _sourceRef.PlayOneShot(clip.voiceline, clip.volumeScale);
     }
     Voiceclip ClipSelector(Voiceclip[] clips)
     {
@@ -52,17 +74,18 @@ public class VoicelineManager : MonoBehaviour
     {
         public AudioClip voiceline;
         public String subtitle;
-        public float VolumeScale;
+        [FormerlySerializedAs("VolumeScale")]
+        public float volumeScale;
     }
 
     [Serializable]
-    class finalClip
+    class FinalClip
     {
         public Voiceclip firstLine;
         public Voiceclip secondLine;
         public Voiceclip finalLine;
 
-        public finalClip(Voiceclip first, Voiceclip second, Voiceclip final)
+        public FinalClip(Voiceclip first, Voiceclip second, Voiceclip final)
         {
             firstLine = first;
             secondLine = second;
