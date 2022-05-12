19ad6345a2f3184386b3ca6f43ad97e4df352f79
jackosullivan88 <jackosullivan88@gmail.com>
2022-05-12 12:16:13 -0400

    more anims, edited text stuff

--- Assets/Scripts/HitboxController.cs
+++ Assets/Scripts/HitboxController.cs
@@ -17,13 +17,7 @@ public class HitboxController : MonoBehaviour
     {
         hitboxCollider = GetComponent<BoxCollider>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
