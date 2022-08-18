5cad1a0fc61b45e70ff873118a0324cdf58c20f7
jackosullivan88 <35341564+jackosullivan88@users.noreply.github.com>
2022-08-18 16:08:20 -0400

    Improved commenting to Dash and DashHitbox scripts

--- Assets/Characters/Universal Scripts/Dash.cs
+++ Assets/Characters/Universal Scripts/Dash.cs
@@ -6,17 +6,22 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    //References changeable in the Inspector
+        //1. Reference to the hitbox object within the player prefab 
     [SerializeField] private BoxCollider hitbox;
+        //2. Animation curve responsible for speed curve of the dash
     [SerializeField] private AnimationCurve lerpCurve;
     
-    
+    //Internal float variables responsible for the actual dash speed  frame-by-frame
     private float dashSpeed, dashSpeedLastFrame;
+    //Bool that tracks if the current dash is past its peak speed
     private bool pastPeakSpeed;
+    //Public floats that determine dash attributes (Default values NOT final)
     public float dashPeakSpeed = 15f;
     public float dashTime = 0.1f;
-
     public float force = 15f;
 
+    //References to player stuff
     private InputProcessor input;
     private PlayerBase playerBase;
     private CharacterController characterController;
@@ -24,17 +29,25 @@ public class Dash : MonoBehaviour
 
     private void Start()
     {
+        //Attaching player stuff references
         input = GetComponent<InputProcessor>();
         playerBase = this.GetComponent<PlayerBase>();
         anim = this.GetComponent<Animator>();
         characterController = this.GetComponent<CharacterController>();
+        //Make hitbox inactive upon Player initialization
         hitbox.gameObject.SetActive(false);
     }
 
     private void Update()
     {
+        /*
+         Upon recieving "dash" input, this script checks the player's current state.
+         If state is "idle" (0) or "move" (1), the DashFunction coroutine begins. 
+         */
+         
         if (playerBase.GetDash())
         {
+            
             if (playerBase.GetState() == 0 || playerBase.GetState() == 1)
             {
                 StartCoroutine(DashFunction());
@@ -44,44 +57,59 @@ public class Dash : MonoBehaviour
 
     public IEnumerator DashFunction()
     {
+        //These lines run upon starting the Dash coroutine
+        //Establish "startTime" as the time as of the coroutine's calling
         float startTime = Time.time;
+        //Get directional input from the player
         Vector3 dashDir = new Vector3(playerBase.dir.x, 0, playerBase.dir.z).normalized;
+        //We haven't passed peak speed yet bc the function just started
         pastPeakSpeed = false;
-        Debug.Log("dashDir: " + dashDir.ToString()); 
+        //Set current player state to "Dash" (2)
         SetAnimState(2);
 
+        //Do this until time goes past "dashTime"
         while (Time.time < startTime + dashTime)
         {
+            //set dashSpeed to dashPeakSpeed * whatever point on the curve we're currently on
             dashSpeed = (dashPeakSpeed * (lerpCurve.Evaluate(Time.time - startTime)));
-            if ((dashSpeed < dashSpeedLastFrame) && pastPeakSpeed == false)
+            
+            //If we're still speeding up, allow the player to change their dash trajectory
+            if (pastPeakSpeed == false)
             {
                 dashDir = new Vector3(playerBase.dir.x, 0, playerBase.dir.z).normalized;
+                //If we're not still speeding up, then set pastPeakSpeed to true
                 if (dashSpeed < dashSpeedLastFrame)
                 {
                     pastPeakSpeed = true;
                 }
             }
-            Debug.Log("DashSpeed: " + dashSpeed);
+            
+            //Actual dash movement sent to the character controller
             characterController.Move(dashDir.normalized * dashSpeed * Time.deltaTime);
+            //At the end of this frame, set whatever the current speed is so it can be checked next frame
             dashSpeedLastFrame = dashSpeed;
             yield return null;
         }
+        //Once dash is over, return to base speed and return to player state "idle" (0)
         playerBase.ReturnToBaseSpeed();
         SetAnimState(0);
     }
     
     void EnableHitbox()
     {
+        //Enables dash hitbox (Referenced in animation)
         hitbox.gameObject.SetActive(true);
     }
     
     void DisableHitbox()
     {
+        //Disables dash hitbox (Referenced in animation)
         hitbox.gameObject.SetActive(false);
     }
 
     void SetAnimState(int newState)
     {
+        //Sets given state in both animator and playerBase
         anim.SetInteger("playerState", newState);
         playerBase.SetState(newState);
     }
--- Assets/Characters/Universal Scripts/DashHitbox.cs
+++ Assets/Characters/Universal Scripts/DashHitbox.cs
@@ -5,34 +5,38 @@ using UnityEngine;
 
 public class DashHitbox : MonoBehaviour
 {
+    /// <summary>
+    /// Attaches to the actual hitbox object within the player prefab.
+    /// Recives info about how it should act from the base player prefab, and implements this when colliding with ball.
+    /// </summary>
+    
+    //Ref to actual collider
     private BoxCollider collider;
-
+    //Ref to "Dash" script attached to base player
     private Dash dashData;
-
+    //Holds velocity of ball when they collide
     private float velRef;
     
-    // Start is called before the first frame update
     void Start()
     {
+        //Attach to refs
         collider = GetComponent<BoxCollider>();
         dashData = collider.transform.root.GetComponent<Dash>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     private void OnTriggerEnter(Collider other)
     {
+        //When the hitbox connects with the ball
         if (other.gameObject.CompareTag("Ball"))
         {
+            //Set velRef to the ball's current speed
             velRef = other.GetComponent<Rigidbody>().velocity.magnitude;
-
+            //Set the direction we're gonna send the ball in to the Player's current facing direction
+            //Multiply the direction vector to the "force" value in the Player's "Dash" variable
             Vector3 newDirection = (transform.root.GetComponent<PlayerBase>().dir) * (dashData.force);
             Vector3 currentVelocity = other.gameObject.GetComponent<Rigidbody>().velocity;
 
+            //Add the force of "newDirection" to the ball
             other.GetComponent<Rigidbody>().AddForce(newDirection, ForceMode.VelocityChange);
         }
     }
